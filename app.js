f254c857fa2033ebfccf32031a583129b064bb68
cai5232 <caicai523261@qq.com>
2026-08-07 21:05:02 +0800

    流式跟底：去掉scrollIntoView，改回scrollTop=scrollHeight+_userScrolled判断

--- app.js
+++ app.js
@@ -2079,8 +2079,11 @@ function xkStreamAppend(block, chunk){
       curPara.insertBefore(document.createTextNode(part),cursor)
     }
   })
-  // 用 cursor 自身 scrollIntoView，iOS 上比 scrollTop=scrollHeight 更可靠
-  cursor.scrollIntoView({block:'nearest',inline:'nearest'})
+
+  const box=document.getElementById('xkStream')
+  if(box&&!box._userScrolled){
+    box.scrollTop=box.scrollHeight
+  }
 }
 
 function xkStreamDone(block){
