90cc0fb08e8c46c57f00debedaa3a1e9c28b20ee
cai5232 <caicai523261@qq.com>
2026-08-10 02:31:04 +0800

    revert: mcpSave去掉KV调用

--- app.js
+++ app.js
@@ -3070,7 +3070,6 @@ let _mcpAddType = 'http'
 
 function mcpSave(){
   localStorage.setItem('mcp_servers', JSON.stringify(_mcpServers))
-  kvPutDebounced('mcp_servers', _mcpServers, 1500)
 }
 
 // 代理请求
